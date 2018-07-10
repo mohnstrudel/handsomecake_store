c0cf58de7301341ef2340c854b83eae50ae81d52
Anton Kostin <schnibba@gmail.com>
2018-07-10 16:17:03 +0300

    Setup all css and js correctly.

---
+++ app/assets/javascripts/front.js
@@ -0,0 +1,12 @@
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+
+//= require front/disrupt
+//= require front/main
+//= require front/contact-form
+//= require front/plugins/lightgallery/lightgallery-all
+//= require front/plugins/lightgallery/init
+//= require front/plugins/isotope/jquery.isotope.min
+//= require front/plugins/isotope/imagesloaded
+//= require front/plugins/isotope/init
